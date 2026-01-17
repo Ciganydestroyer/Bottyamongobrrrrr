5d0bc7e53a418334acc4cd4483e2b0e90a59dbb2
Aron <aron20081009@gmail.com>
2026-01-17 20:55:28 +0100

    added new package also made an indicator which button is the user on

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-import keyboard
 from button import Buttons
+import packages.keyboard
+from packages.colorama import Fore,Back,init
+init(autoreset=True)
+import time
+import os
 
 class Player:
     def __init__(self, hp, attack, defense, speed, critchance, lvl):
@@ -11,41 +14,58 @@ class Player:
         self.critchance = critchance
         self.lvl = lvl
 
+def renderer():
+    clear = lambda: os.system('cls')
+    clear()
 
+    Bottyamon_MainTitle()
+    Start.Align_Center()
+    Start.CreateButton()
+    TEST1.Align_Center()
+    TEST1.CreateButton()
+    TEST2.Align_Center()
+    TEST2.CreateButton()
 
+def Bottyamon_MainTitle():
+    print(r"+-------------------------------------------------------------------------------------------------------+")
+    print(r"|                                                                                                       |")
+    print(r"|      /$$$$$$$              /$$     /$$                                                                |")
+    print(r"|     | $$__  $$            | $$    | $$                                                                |")
+    print(r"|     | $$  \ $$  /$$$$$$  /$$$$$$ /$$$$$$   /$$   /$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$  /$$$$$$$       |")
+    print(r"|     | $$$$$$$  /$$__  $$|_  $$_/|_  $$_/  | $$  | $$ |____  $$| $$_  $$_  $$ /$$__  $$| $$__  $$      |")
+    print(r"|     | $$__  $$| $$  \ $$  | $$    | $$    | $$  | $$  /$$$$$$$| $$ \ $$ \ $$| $$  \ $$| $$  \ $$      |")
+    print(r"|     | $$  \ $$| $$  | $$  | $$ /$$| $$ /$$| $$  | $$ /$$__  $$| $$ | $$ | $$| $$  | $$| $$  | $$      |")
+    print(r"|     | $$$$$$$/|  $$$$$$/  |  $$$$/|  $$$$/|  $$$$$$$|  $$$$$$$| $$ | $$ | $$|  $$$$$$/| $$  | $$      |")
+    print(r"|     |_______/  \______/    \___/   \___/   \____  $$ \_______/|__/ |__/ |__/ \______/ |__/  |__/      |")
+    print(r"|                                            /$$  | $$                                                  |")
+    print(r"|                                           |  $$$$$$/                                                  |")
+    print(r"|                                            \______/                                                   |")
+    print(r"|                                                                                                       |")
+    print(r"+-------------------------------------------------------------------------------------------------------+")
+    print("\n")
 
-
-print(r"+-------------------------------------------------------------------------------------------------------+")
-print(r"|                                                                                                       |")
-print(r"|      /$$$$$$$              /$$     /$$                                                                |")
-print(r"|     | $$__  $$            | $$    | $$                                                                |")
-print(r"|     | $$  \ $$  /$$$$$$  /$$$$$$ /$$$$$$   /$$   /$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$  /$$$$$$$       |")
-print(r"|     | $$$$$$$  /$$__  $$|_  $$_/|_  $$_/  | $$  | $$ |____  $$| $$_  $$_  $$ /$$__  $$| $$__  $$      |")
-print(r"|     | $$__  $$| $$  \ $$  | $$    | $$    | $$  | $$  /$$$$$$$| $$ \ $$ \ $$| $$  \ $$| $$  \ $$      |")
-print(r"|     | $$  \ $$| $$  | $$  | $$ /$$| $$ /$$| $$  | $$ /$$__  $$| $$ | $$ | $$| $$  | $$| $$  | $$      |")
-print(r"|     | $$$$$$$/|  $$$$$$/  |  $$$$/|  $$$$/|  $$$$$$$|  $$$$$$$| $$ | $$ | $$|  $$$$$$/| $$  | $$      |")
-print(r"|     |_______/  \______/    \___/   \___/   \____  $$ \_______/|__/ |__/ |__/ \______/ |__/  |__/      |")
-print(r"|                                            /$$  | $$                                                  |")
-print(r"|                                           |  $$$$$$/                                                  |")
-print(r"|                                            \______/                                                   |")
-print(r"|                                                                                                       |")
-print(r"+-------------------------------------------------------------------------------------------------------+")
-
-print("\n")
-
+Bottyamon_MainTitle()
 
 Start = Buttons("START",20,5,True)
 
-Start.Align_Center()
-Start.CreateButton()
 
-Settings = Buttons("SETTINGS",21,5,False)
+TEST1 = Buttons("OPTIONS",20,5,False)
+TEST1.Align_Center()
+TEST1.CreateButton()
 
-Settings.Align_Center()
-Settings.CreateButton()
+TEST2 = Buttons("QUIT",20,5,False)
+TEST2.Align_Center()
+TEST2.CreateButton()
 
-Quit = Buttons("QUIT",21,5,False)
+Current_Menu = [Start,TEST1,TEST2]
 
-Quit.Align_Center()
-Quit.CreateButton()
+for i in range(0, len(Current_Menu)):
+    if(Current_Menu[i].default == True):
+        Current_Menu[i].text += " <="
+    else:
+        Current_Menu[i].text.replace("<=","")
 
+
+while True:
+    renderer()
+    time.sleep(0.5)
