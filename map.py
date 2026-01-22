37600843d1f7c35b2214cc5bcc4a48b525b1c014
nagyaron <aron20081009@gmail.com>
2026-01-22 09:35:21 +0100

    Parsed the map somehow??

--- map.py
+++ map.py
@@ -1,1 +1,31 @@
-##TODO: add a pro way to parse the map in types.txt
+class Map:
+    def __init__(self,water):
+        self.water = water
+
+    def Parse_Map(self):
+        file = open("types.txt","r")
+
+        map = []
+
+        for i in file:
+            map.append(i.replace("\n","").split("@"))
+
+
+        return map
+
+    def Parse_Map_Electric_Bogoloo(self):
+        map = self.Parse_Map()
+        bettermap = []
+        temp = []
+
+        for i in map:
+            temp.append(i)
+            if(i.count('') == 2):
+                bettermap.append(temp)
+                temp = []
+
+        return bettermap
+
+fasz = Map(False)
+
+what = fasz.Parse_Map_Electric_Bogoloo()
